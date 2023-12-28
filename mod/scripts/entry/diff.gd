90c098484c56db80e0b6174b2345f215f5afabf5
NomikuH8 <sofaco.checkin@gmail.com>
2023-12-28 09:44:26 -0300

    created some scaffolding for gameplay

--- mod/scripts/entry/diff.gd
+++ mod/scripts/entry/diff.gd
@@ -5,6 +5,13 @@ var infos: Dictionary = {}
 
 
 func _ready():
-	print(infos)
 	%SongTitleLabel.text = infos["Metadata"]["TitleUnicode"]
 	%DiffNameLabel.text = infos["Metadata"]["Version"]
+
+
+func _on_play_button_pressed():
+	var mod = ModManager.loaded_mods.filter(func(m): return m["file_name"] == "osu_standard")[0]
+	var mod_path = "res://modules/" + mod["file_name"]
+	var scene_path = mod_path + "/scenes/gameplay/gameplay.tscn"
+	
+	SceneChanger.goto_scene(scene_path, { "diff": infos })
